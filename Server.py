554e2e4f5db11afc8687390adbac7795dc5d5a0b
Znerual <laurenz_ruzicka@gmx.at>
2018-08-22 12:13:39 +0200

    Code Cleanup + Comments, everythink working for now

--- Server.py
+++ Server.py
@@ -9,18 +9,14 @@ server_socket.bind((socket.gethostbyname(socket.gethostname()), 5555))
 while True:
     state = 0
     private_key, public_key = generate_keyset()
+    
     #Verbinde mit Clienten und erhalten den Socket
     server_socket.listen(5)
     client_serving_socket, addr = server_socket.accept()
     print("Verbunden")
+
     #Warte auf den Public_key des Clienten
     keyData = recv_data(client_serving_socket)
-
-    #while True:
-    #    daten = client_serving_socket.recv(2048)
-    #    if not daten: break
-    #    keyData += daten
-
     print("Key empfangen")
     client_public_key = keyData
     print(keyData)
@@ -28,15 +24,13 @@ while True:
     #Sende Nachricht und warte auf Antwort
     message = encrypt_session("Hey du", client_public_key)
     send_data(pickle.dumps(message), client_serving_socket)
-    #client_serving_socket.send(pickle.dumps(message))
-
-    #client_serving_socket.send(b"00000000")
     print("Nachricht gesendet")
 
-    #data, addrInfo = client_serving_socket.recvfrom(2048)
-
+    #Warte auf bestätigung
     text = recv_text(client_serving_socket)
     print(text)
+
+    #beende die Verbindung
     client_serving_socket.close()
     del client_serving_socket
     break
--- timeClient.py
+++ timeClient.py
@@ -6,26 +6,16 @@ from protocoll import send_data, recv_data, send_text
 server_addr = (socket.gethostbyname(socket.gethostname()), 5555)
 #connect to the server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#client_socket.settimeout(7)
 client_socket.connect(server_addr)
 
 #generate the keyset
 private_key, public_key = generate_keyset()
 
 #send the public key to the server
-
 send_data(public_key, client_socket)
-#client_socket.send(public_key)
 
 #wait for encrypted response
 messageData = recv_data(client_socket)
-
-#while True:
-#    daten, addr = client_socket.recvfrom(1024)
-#    messageData += daten
-#    if not daten:
-#        break
-
 enc_message = pickle.loads(messageData)
 message, session_key = decrypt_session(enc_message, private_key)
 print(message)
